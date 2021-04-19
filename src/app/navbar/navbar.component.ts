69c2dd6531c127e9b98523ddb2bf65dc68541020
averak <abe12@mccc.jp>
2021-04-19 21:12:44 +0900

    [feat] 動的に表示を切り替える

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -6,7 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.css'],
 })
 export class NavbarComponent implements OnInit {
+  isMenuVisible = false;
+  isUserMenuVisible = false;
+
   constructor() {}
 
   ngOnInit(): void {}
+
+  pressMenu(): void {
+    this.isMenuVisible = !this.isMenuVisible;
+  }
+
+  pressUserMenu(): void {
+    this.isUserMenuVisible = !this.isUserMenuVisible;
+  }
 }
