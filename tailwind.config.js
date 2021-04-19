8073326e17f21f2ce497745a684cf79711eefc8e
averak <abe12@mccc.jp>
2021-04-19 18:45:51 +0900

    [chore] require tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  purge: {
+    content: ['./src/**/*.{html,ts}'],
+  },
+  darkMode: false,
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
